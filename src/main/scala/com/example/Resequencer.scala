3a5a58a808fc0aef562ea64d04ac0c17ec80f126
shigemk2 <i.am.shige@gmail.com>
2016-11-27 22:42:57 +0900

    Add case classes

--- src/main/scala/com/example/Resequencer.scala
+++ src/main/scala/com/example/Resequencer.scala
@@ -2,5 +2,13 @@ package com.example
 
 import akka.actor._
 
+case class SequencedMessage(correlationId: String, index: Int, total: Int)
+
+case class ResequencedMessages(dispatchableIndex: Int, sequencedMessages: Array[SequencedMessage]) {
+  def advancedTo(dispatchableIndex: Int) = {
+    ResequencedMessages(dispatchableIndex, sequencedMessages)
+  }
+}
+
 object ResequencerDriver extends CompletableApp(10) {
 }
