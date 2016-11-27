67ee51b7e18b65f5ebaa7c01bed1ed009ccc1c65
shigemk2 <i.am.shige@gmail.com>
2016-11-28 01:26:11 +0900

    Add ChaosRouter

--- src/main/scala/com/example/Resequencer.scala
+++ src/main/scala/com/example/Resequencer.scala
@@ -1,8 +1,11 @@
 package com.example
 
+import java.util.concurrent.TimeUnit
 import java.util.{Date, Random}
 
 import akka.actor._
+
+import scala.concurrent.duration.Duration
 
 case class SequencedMessage(correlationId: String, index: Int, total: Int)
 
@@ -17,4 +20,14 @@ object ResequencerDriver extends CompletableApp(10) {
 
 class ChaosRouter(consumer: ActorRef) extends Actor {
   val random = new Random((new Date()).getTime)
+
+  def receive = {
+    case sequencedMessage: SequencedMessage =>
+      val millis = random.nextInt(100) + 1
+      println(s"ChaosRouter: delaying delivery of $sequencedMessage for $millis milliseconds")
+      val duration = Duration.create(millis, TimeUnit.MILLISECONDS)
+      context.system.scheduler.scheduleOnce(duration, consumer, sequencedMessage)
+    case message: Any =>
+      println(s"ChaosRouter: received unexpected: $message")
+  }
 }
