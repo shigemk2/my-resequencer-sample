20b02088970d5ba4d8fb1d70322f1c0bed0e7f60
shigemk2 <i.am.shige@gmail.com>
2016-12-05 02:24:45 +0900

    Fix

--- src/main/scala/com/example/Resequencer.scala
+++ src/main/scala/com/example/Resequencer.scala
@@ -21,18 +21,17 @@ object ResequencerDriver extends CompletableApp(10) {
   val sequencedMessageConsumer = system.actorOf(Props[SequencedMessageConsumer], "sequencedMessageConsumer")
   val resequencerConsumer = system.actorOf(Props(classOf[ResequencerConsumer], sequencedMessageConsumer), "resequencerConsumer")
   val chaosRouter = system.actorOf(Props(classOf[ChaosRouter], resequencerConsumer), "chaosRouter")
-
+  
   for (index <- 1 to 5) chaosRouter ! SequencedMessage("ABC", index, 5)
   for (index <- 1 to 5) chaosRouter ! SequencedMessage("XYZ", index, 5)
-
+  
   awaitCompletion
   println("Resequencer: is completed.")
 }
 
 class ChaosRouter(consumer: ActorRef) extends Actor {
-
-  val random = new Random(new Date().getTime)
-
+  val random = new Random((new Date()).getTime)
+  
   def receive = {
     case sequencedMessage: SequencedMessage =>
       val millis = random.nextInt(100) + 1
@@ -50,20 +49,19 @@ class ResequencerConsumer(actualConsumer: ActorRef) extends Actor {
   def dispatchAllSequenced(correlationId: String) = {
     val resequencedMessages = resequenced(correlationId)
     var dispatchableIndex = resequencedMessages.dispatchableIndex
-
-    resequencedMessages.sequencedMessages.foreach { sequenceMessage =>
-
-      if (sequenceMessage.index == dispatchableIndex) {
-        actualConsumer ! sequenceMessage
-
+    
+    resequencedMessages.sequencedMessages.foreach { sequencedMessage =>
+      if (sequencedMessage.index == dispatchableIndex) {
+        actualConsumer ! sequencedMessage
+        
         dispatchableIndex += 1
       }
     }
-
+    
     resequenced(correlationId) =
       resequencedMessages.advancedTo(dispatchableIndex)
   }
-
+  
   def dummySequencedMessages(count: Int): Seq[SequencedMessage] = {
     for {
       index <- 1 to count
@@ -71,8 +69,8 @@ class ResequencerConsumer(actualConsumer: ActorRef) extends Actor {
       SequencedMessage("", -1, count)
     }
   }
-
-  override def receive: Receive = {
+  
+  def receive = {
     case unsequencedMessage: SequencedMessage =>
       println(s"ResequencerConsumer: received: $unsequencedMessage")
       resequence(unsequencedMessage)
@@ -81,23 +79,25 @@ class ResequencerConsumer(actualConsumer: ActorRef) extends Actor {
     case message: Any =>
       println(s"ResequencerConsumer: received unexpected: $message")
   }
-
+  
   def removeCompleted(correlationId: String) = {
     val resequencedMessages = resequenced(correlationId)
-
+    
     if (resequencedMessages.dispatchableIndex > resequencedMessages.sequencedMessages(0).total) {
       resequenced.remove(correlationId)
       println(s"ResequencerConsumer: removed completed: $correlationId")
     }
   }
-
+  
   def resequence(sequencedMessage: SequencedMessage) = {
     if (!resequenced.contains(sequencedMessage.correlationId)) {
       resequenced(sequencedMessage.correlationId) =
         ResequencedMessages(1, dummySequencedMessages(sequencedMessage.total).toArray)
     }
-
+    
     resequenced(sequencedMessage.correlationId)
+    	.sequencedMessages
+    	.update(sequencedMessage.index - 1, sequencedMessage)
   }
 }
 
@@ -108,4 +108,5 @@ class SequencedMessageConsumer extends Actor {
       ResequencerDriver.completedStep()
     case message: Any =>
       println(s"SequencedMessageConsumer: received unexpected: $message")
-  }}
+  }
+}
